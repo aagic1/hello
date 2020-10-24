0361f4edf90c5d27f2e6d30e486ae7c9e6c487be
Ahmed Agic <aagic1@etf.unsa.ba>
2020-10-24 21:41:35 +0200

    Promjena teksta koji se ispisuje

--- src/ba/unsa/etf/rpr/MojaPrvaKlasa.java
+++ src/ba/unsa/etf/rpr/MojaPrvaKlasa.java
@@ -3,6 +3,6 @@ package ba.unsa.etf.rpr;
 public class MojaPrvaKlasa {
 
     public static void main(String[] args) {
-        System.out.println("Dobrodosli na rpr!");
+        System.out.println("Pozdrav svima koji prate!");
     }
 }
