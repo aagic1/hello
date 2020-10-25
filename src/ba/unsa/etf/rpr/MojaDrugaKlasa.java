31bcef63a22ae7ce81a73c289ad798cafc6ca101
Ahmed Agic <aagic1@etf.unsa.ba>
2020-10-25 14:45:52 +0100

    Dodana metoda ispisiJosNesto u klasu MojaDrugaKlasa, koja ispisje "Ispis jos nesto." i upotrijebljena u klasi MojaPrvaKlasa.

--- src/ba/unsa/etf/rpr/MojaDrugaKlasa.java
+++ src/ba/unsa/etf/rpr/MojaDrugaKlasa.java
@@ -3,5 +3,9 @@ package ba.unsa.etf.rpr;
 public class MojaDrugaKlasa {
     public static void ispisiNesto(){
         System.out.println("Nesto.");
+
+    }
+    public static void ispisiJosNesto(){
+        System.out.println("Ispisi jos nesto.");
     }
 }
--- src/ba/unsa/etf/rpr/MojaPrvaKlasa.java
+++ src/ba/unsa/etf/rpr/MojaPrvaKlasa.java
@@ -7,5 +7,6 @@ public class MojaPrvaKlasa {
     public static void main(String[] args) {
         System.out.println("Pozdrav svima koji prate!");
         ispisiNesto();
+        ispisiJosNesto();
     }
 }
