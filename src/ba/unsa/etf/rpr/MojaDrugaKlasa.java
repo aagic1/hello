73184db4a06933b406d5c822cd05f009e9f4006e
Ahmed Agic <aagic1@etf.unsa.ba>
2020-10-25 14:41:45 +0100

    Dodana metoda ispisiNesto u klasi MojaDrugaKlasa koja ispisuje "nesto", koja je iskoristena u klasi MojaPrvaKlasa.

--- src/ba/unsa/etf/rpr/MojaDrugaKlasa.java
+++ src/ba/unsa/etf/rpr/MojaDrugaKlasa.java
@@ -1,4 +1,7 @@
 package ba.unsa.etf.rpr;
 
 public class MojaDrugaKlasa {
+    public static void ispisiNesto(){
+        System.out.println("Nesto.");
+    }
 }
--- src/ba/unsa/etf/rpr/MojaPrvaKlasa.java
+++ src/ba/unsa/etf/rpr/MojaPrvaKlasa.java
@@ -1,8 +1,11 @@
 package ba.unsa.etf.rpr;
 
+import static ba.unsa.etf.rpr.MojaDrugaKlasa.*;
+
 public class MojaPrvaKlasa {
 
     public static void main(String[] args) {
         System.out.println("Pozdrav svima koji prate!");
+        ispisiNesto();
     }
 }
